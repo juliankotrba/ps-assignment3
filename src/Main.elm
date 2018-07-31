be57a4d8cb1d1e35ce87a36583896a1b65e9e303
Julian Kotrba <me@juliankotrba.xyz>
2018-07-31 17:19:08 +0200

    Create basic Elm project

---
+++ src/Main.elm
@@ -0,0 +1,73 @@
+import Html exposing (Html, div, input, textarea, text, span)
+import Html.Attributes exposing (..)
+
+main = Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+--vtype CodeSequence = Wrapped String | Normal String
+
+type alias Model = { plainSourceCode: String  }
+
+model : Model
+model = Model "fun isFunny() = False"
+
+-- UPDATE
+
+type Msg = Reset
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Reset -> { model | plainSourceCode = "..." }
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model = Html.div [ mainContainerStyle ] [
+  div [ containerStyle ]
+  [
+    div [  ] [
+      textarea [ textareaStyle, rows 10, cols 50, placeholder "Write code here" ] []
+    ],
+    div [ formattedCodeContainerStyle ] [
+      span [ ] [ text "code sequence .."],
+      span [ ] [ text ".. code sequence .."],
+      span [ ] [ text ".. code sequence .."],
+      span [ ] [ text ".. code sequence .."]
+    ]
+  ]
+ ]
+
+textareaStyle : Html.Attribute msg
+textareaStyle =
+  style
+   [
+     ("resize", "none")
+   ]
+
+mainContainerStyle : Html.Attribute msg
+mainContainerStyle =
+  style
+    [ ("text-align", "center")
+    , ("backgroundColor", "#263238")
+    , ("height", "100%")
+    , ("width", "100%")
+    ]
+
+containerStyle : Html.Attribute msg
+containerStyle =
+  style
+    [ ("height", "100%")
+    , ("width", "300px")
+    , ("margin-top", "50px")
+    , ("display", "inline-block")
+    ]
+
+formattedCodeContainerStyle : Html.Attribute msg
+formattedCodeContainerStyle =
+  style
+    [ ("text-align", "left")
+    , ("color", "white")
+    ]
