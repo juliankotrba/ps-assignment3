23220b5a62ceed01a6075006f958d20ef9de8f75
Julian Kotrba <me@juliankotrba.xyz>
2018-08-04 11:10:54 +0200

    Add seq implementation

--- src/Parser.elm
+++ src/Parser.elm
@@ -7,15 +7,13 @@ import Char
 
 type alias Parse a b  = List a -> List (b, List a)
 
--- Primitives
+-- Parser primitives
 
 succeed : b -> Parse a b
 succeed val inp = [(val, inp)]
 
 none : Parse a b
 none _ = []
-
--- Parser combinators
 
 token : a -> Parse a a
 token t l = spot (\x -> x==t) l
@@ -27,12 +25,37 @@ spot p l = case l of
     else (none l)
   _ ->  none l
 
+-- Parser combinators
+
 alt : Parse a b -> Parse a b -> Parse a b
 alt p1 p2 inp = p1 inp ++ p2 inp
+
+-- TODO (>*>) : Parse a b -> Parse a c -> Parse a (b,c)
+--              infixr 9 >*>
+
+infixr 9 >*>
+seq : Parse a b -> Parse a c -> Parse a (b,c)
+seq p1 p2 inp
+        =
+          let
+            res1 = p1 inp
+          in
+            case res1 of
+              (val1, rem1)::_ ->  let
+                                    res2 = p2 rem1
+                                  in
+                                    case res2 of
+                                      (val2, rem2)::_ -> [((val1, val2), rem2)]
+                                      _ -> []
+
+              _ -> []
+
+-- Parser
 
 digit : Parse Char Char
 digit = spot Char.isDigit
 
+letter : Parse Char Char
 letter = spot isLetter
 
 isLetter : Char -> Bool
