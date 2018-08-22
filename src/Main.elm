2f921140f9101684f02cd344c6afd60f9899ea11
Julian Kotrba <me@juliankotrba.xyz>
2018-08-22 18:34:22 +0200

    Add open file (over http) feature
    You can use the following hosted dummy source code:
     - https://juliankotrba.xyz/files/demo_code.txt

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,61 @@
-import Html exposing (Html, div, input, textarea, text, span)
-import Html.Events exposing(onInput)
+import Html exposing (Html, div, input, textarea, text, span, button)
+import Html.Events exposing(onInput, onClick)
 import Html.Attributes exposing (..)
 import Json.Decode
 import Parser
+import Http
 
-main = Html.beginnerProgram { model = model, view = view, update = update }
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
 
--- MODEL
-
---vtype CodeSequence = Wrapped String | Normal String
+-- Model
 
 type alias Model =
   { plainSourceCode: String
   , splitSourceCode: List (Html Msg)
+  , url: String
   }
 
-model : Model
-model = Model "" []
+init : (Model, Cmd Msg)
+init =
+  ({ plainSourceCode = ""
+   , splitSourceCode = []
+   , url = ""
+  }
+  , Cmd.none
+  )
 
--- UPDATE
+-- Update
 
 type Msg
   = Reset
   | OnSpanClick String
   | OnCodeInput String
+  | OnLoadCode
+  | OnCodeLoaded (Result Http.Error String)
+  | OnUrlInput String
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Reset -> { model | plainSourceCode = "", splitSourceCode = [] }
+    Reset -> ({ model | plainSourceCode = "", splitSourceCode = [] }, Cmd.none)
     OnSpanClick innerText ->
-      { model | splitSourceCode = (markSameOccurrences innerText (splitSourceCode model.plainSourceCode )) }
+      ({ model | splitSourceCode = [] {- (markSameOccurrences innerText (splitSourceCode model.plainSourceCode )) -} }, Cmd.none)
+
     OnCodeInput changedCode ->
-      { model | plainSourceCode = changedCode, splitSourceCode =
-        List.map (\s->createUnmarkedSpan s) (splitSourceCode model.plainSourceCode)}
+      ({ model | plainSourceCode = changedCode, splitSourceCode = [(createMarkedSpan changedCode)]
+        {-List.map (\s->createUnmarkedSpan s) (splitSourceCode model.plainSourceCode)-} } , Cmd.none)
+
+    OnUrlInput url ->  ({ model | url = url }, Cmd.none)
+    OnLoadCode -> (model, Http.send OnCodeLoaded (Http.getString model.url))
+    OnCodeLoaded (Ok code) -> update (OnCodeInput code) model -- ({ model | plainSourceCode = code }, Cmd.none)
+    OnCodeLoaded (Err _) -> ({ model | plainSourceCode = "Loading failed" }, Cmd.none)
 
 
 splitSourceCode : String -> List String
@@ -56,16 +78,23 @@ view model = Html.div [ mainContainerStyle ] [
   div [ containerStyle ]
   [
     div [  ] [
-      textarea [  textareaStyle, rows 10, cols 50, onInput OnCodeInput ] [  ]
+      div [] [
+        input [ urlInputStyle, placeholder "Load a file over http ..", onInput OnUrlInput] [ ],
+        button [ loadButtonStyle , onClick OnLoadCode ] [ text "Load" ]
+      ],
+      textarea [  textareaStyle, rows 10, onKeyUp OnCodeInput ] [ text model.plainSourceCode ]
     ],
     div [ formattedCodeContainerStyle ] (model.splitSourceCode)
   ]
  ]
 
-
 onSpanClick : (String -> msg) -> Html.Attribute msg
 onSpanClick tagger =
   Html.Events.on "click" (Json.Decode.map tagger decodeInnerTextAttr)
+
+onKeyUp : (String -> msg) -> Html.Attribute msg
+onKeyUp tagger =
+  Html.Events.on "keyup" (Json.Decode.map tagger decodeValueAttr)
 
 decodeInnerTextAttr : Json.Decode.Decoder String
 decodeInnerTextAttr =
@@ -75,14 +104,13 @@ decodeValueAttr : Json.Decode.Decoder String
 decodeValueAttr =
   Json.Decode.at [ "target", "value" ] Json.Decode.string
 
-
--- CSS STYLES
+-- CSS styles
 
 textareaStyle : Html.Attribute msg
 textareaStyle =
   style
-   [
-     ("resize", "none")
+   [ ("resize", "none")
+   , ("width", "100%")
    ]
 
 mainContainerStyle : Html.Attribute msg
@@ -98,7 +126,7 @@ containerStyle : Html.Attribute msg
 containerStyle =
   style
     [ ("height", "100%")
-    , ("width", "300px")
+    , ("width", "400px")
     , ("margin-top", "50px")
     , ("display", "inline-block")
     ]
@@ -124,3 +152,38 @@ unmarkedSpanStyle =
     [ ("color", "white")
     , ("font-family", "monospace")
     ]
+
+urlContainerStyle : Html.Attribute msg
+urlContainerStyle =
+  style
+    [ {- TODO -} ]
+
+urlInputStyle : Html.Attribute msg
+urlInputStyle =
+  style
+    [ ("width", "250px")
+    , ("float", "left")
+    ]
+
+loadButtonStyle : Html.Attribute msg
+loadButtonStyle =
+    style
+      [ ("width", "140px")
+      , ("float", "right")
+      ]
+
+-- Helper functions
+
+splitIntoRules s = splitAndKeep "." s
+
+splitAndKeep : String -> String -> List String
+splitAndKeep d s
+  = String.split d s |> List.filter ((/=) "") |> List.map (\s->s++d)
+
+code ="""
+main-:findFiles-(*files):showFiles(*files)-.
+showFiles(+x*x)-:showFile(+x)-:showFiles(*x)-.
+showFiles()-.
+findFiles-(*f):$(ls \\*\\.pdf)()-(*f)(*x).
+showFile(+f)-:$(xpdf+f)()-(*a)(+b).
+"""
